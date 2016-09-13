3c0047fa21425e4b17b9e04997ea326573f1fcac
Patrick Hempel <patrick@hempel.io>
2016-09-13 09:44:28 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,80 @@
+'use strict';
+const express   = require('express');
+const app       = express();
+const cors      = require('express-cors');
+const request   = require('request');
+const _         = require('lodash');
+const Q         = require('Q');
+
+const data = {
+    'tos': 'https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Star_Trek:_The_Original_Series_episodes&cmlimit=500&format=json',
+    'tng': 'https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Star_Trek:_The_Next_Generation_episodes&cmlimit=500&format=json',
+    'dsn': 'https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Star_Trek:_Deep_Space_Nine_episodes&cmlimit=500&format=json',
+    'voy': 'https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Star_Trek:_Voyager_episodes&cmlimit=500&format=json',
+    'ent': 'https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Star_Trek:_Enterprise_episodes&cmlimit=500&format=json'
+};
+
+const startUpPromises = [];
+
+_.map( Object.keys( data), k => {
+    let defer = Q.defer();
+
+    console.log( 'fetching episodes for ' + k );
+
+    request( data[k], (error, response, body) => {
+        var body = JSON.parse( body);
+        data[k] = body.query.categorymembers;
+        defer.resolve();
+    });
+
+    startUpPromises.push( defer.promise);
+});
+
+
+const startApp = () => {
+    app.use( cors({
+        allowedOrigins: [ 'localhost:*' ],
+        headers: [ 'authorization' ]
+    }));
+
+    app.get( '/random', (req, res, next) => {
+        let item = data.all[Math.floor(Math.random()*data.all.length)];
+
+        let response = {
+            "text": item.title
+        };
+
+        return res.send( response);
+    });
+
+
+    app.listen(3000, function() {
+        console.log('Example app listening on port 3000!');
+    });
+};
+
+const fetchUrl = pageid => {
+    request(
+        `https://en.wikipedia.org/w/api.php?action=query&prop=info&pageids=${pageid}&inprop=url&format=json`,
+        (err, response, body) => {
+            let data = JSON.parse( body);
+            console.log( data.query.pages[pageid].fullurl);
+        }
+    )
+};
+
+
+Q.all( startUpPromises)
+    .then( () => {
+        let defer = Q.defer();
+
+        data['all'] = _.reduce( data, (m, i) => m.concat(i));
+
+        defer.resolve();
+
+        return defer.promise;
+    })
+    .then( startApp)
+    .catch( console.error);
+
+
